7f5ebc71ae9993fb090a859db7be8b7a708abdbb
RupaDey404 <rupa121353@gmail.com>
2025-07-24 15:55:47 +0600

    simple mistake is updated in this code

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -17,39 +17,46 @@ public class SeleniumTest {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
        // driver.get("https://www.youtube.com/");
-        Thread.sleep(2000);
+       // Thread.sleep(2000);
 
+        //load the daraz website.
         driver.navigate().to("https://www.daraz.com.bd/");
         //driver.manage().window().maximize();
         Thread.sleep(2000);
 
+        //check the title of this site.
         String title = driver.getTitle();
         if(title.equalsIgnoreCase("daraz"))
             System.out.println("Title matches");
         else System.out.println(title);
 
+        //click on the slider and go to the next page which is category page.
         WebElement slider = driver.findElement(By.cssSelector(".swiper-slide.swiper-slide-active a"));
         slider.click();
         Thread.sleep(2000);
 
+        //Select the specific category with hover: E.g. category>groceries>breakfast>honey>new page open
         WebElement category = driver.findElement(By.cssSelector("div.lzd-site-nav-menu.lzd-site-nav-menu-active"));
         Actions actions = new Actions(driver);
-        actions.moveToElement(category).perform();
+        actions.moveToElement(category).pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
-        WebElement item = driver.findElement(By.xpath("//span[text()='Groceries']"));
+        WebElement item = driver.findElement    (By.xpath("//span[text()='Groceries']"));
         actions.moveToElement(item);
         actions.click();
-        actions.perform();
+        actions.pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
 
-        WebElement breakfast = driver.findElement(By.xpath("//span[text() ='Breakfast')]"));
+        WebElement breakfast = driver.findElement(By.xpath("//a[@class='lzd-site-menu-root-item-link']//span[contains(text(),'Breakfast')]"));
         actions.moveToElement(breakfast);
         actions.click();
-        actions.perform();
+        actions.pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
 
-        driver.findElement(By.linkText("Honey")).click();
+        WebElement honey = driver.findElement(By.xpath("//li[@class='lzd-site-menu-grand-item']//span[contains(text(),'Honey')]"));
+        actions.moveToElement(honey).pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
+
+        System.out.println("navigated successfully");
 
         driver.quit();
     }
