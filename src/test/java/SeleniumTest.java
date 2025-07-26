d36dc49f5bcd0872a94c6fb7ae98eaf316d54b83
RupaDey404 <rupa121353@gmail.com>
2025-07-26 10:56:00 +0600

    dropdown list path is updated and search an item from searchbar.

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -15,9 +16,9 @@ public class SeleniumTest {
     void setup() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
         driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
-       // driver.get("https://www.youtube.com/");
-       // Thread.sleep(2000);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        // driver.get("https://www.youtube.com/");
+        // Thread.sleep(2000);
 
         //load the daraz website.
         driver.navigate().to("https://www.daraz.com.bd/");
@@ -36,7 +37,7 @@ public class SeleniumTest {
         Thread.sleep(2000);
 
         //Select the specific category with hover: E.g. category>groceries>breakfast>honey>new page open
-        WebElement category = driver.findElement(By.cssSelector("div.lzd-site-nav-menu.lzd-site-nav-menu-active"));
+        WebElement category = driver.findElement(By.cssSelector(".lzd-site-menu-nav-category-text"));
         Actions actions = new Actions(driver);
         actions.moveToElement(category).pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
@@ -48,15 +49,32 @@ public class SeleniumTest {
 
         WebElement breakfast = driver.findElement(By.xpath("//a[@class='lzd-site-menu-root-item-link']//span[contains(text(),'Breakfast')]"));
         actions.moveToElement(breakfast);
-        actions.click();
+//        actions.click();
         actions.pause(Duration.ofSeconds(2)).perform();
         Thread.sleep(2000);
 
-        WebElement honey = driver.findElement(By.xpath("//li[@class='lzd-site-menu-grand-item']//span[contains(text(),'Honey')]"));
-        actions.moveToElement(honey).pause(Duration.ofSeconds(2)).perform();
+        WebElement honey = driver.findElement(By.xpath("//a[@href='//www.daraz.com.bd/catalog?q=Honey&from=hp_categories&src=all_channel']//span[contains(text(),'Honey')]"));
+        actions.moveToElement(honey).click().pause(Duration.ofSeconds(2)).perform();
+
         Thread.sleep(2000);
 
         System.out.println("navigated successfully");
+
+        //to clear the search box
+        WebElement searchBox = driver.findElement(By.xpath("//input[@id='q']"));
+//        searchBox.clear(); -->it does not work successfully.
+        searchBox.click();
+        searchBox.sendKeys(Keys.CONTROL + "a");
+        searchBox.sendKeys(Keys.DELETE);
+        Thread.sleep(2000);
+
+        //write the searching element in the search bar:
+        searchBox.sendKeys("Mouse");
+
+        driver.findElement(By.xpath("//a[normalize-space()='SEARCH']")).click();
+        Thread.sleep(2000);
+
+        System.out.println("successfully searched");
 
         driver.quit();
     }
