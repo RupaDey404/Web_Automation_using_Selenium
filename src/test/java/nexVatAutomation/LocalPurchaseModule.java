88cf0a36dac40added6ac3eced3ea9a8fa8ec891
RupaDey404 <rupa121353@gmail.com>
2025-08-14 15:39:41 +0600

    LocalPurchaseModule AND LocalPuchaseTestData files are updated

--- src/test/java/nexVatAutomation/LocalPurchaseModule.java
+++ src/test/java/nexVatAutomation/LocalPurchaseModule.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
@@ -20,26 +21,52 @@ public class LocalPurchaseModule {
 
         driver = LoginPage.driver;
         driver.manage().window().maximize();
-        WebElement purchase = driver.findElement(By.xpath("//span[@title='Purchase']"));
+
         Actions actions = new Actions(driver);
-        actions.moveToElement(purchase).pause(Duration.ofSeconds(2)).click();
-//        actions.pause(Duration.ofSeconds(2)).perform();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        selectWebElement element = new selectWebElement();
+        element.selectElement(driver,actions,LocalPurchaseTestData.path1);
         Thread.sleep(2000);
 
         //-----------Purchase-> Local Purchase-> New Local Purchase
-        WebElement localPurchase = driver.findElement(By.xpath("//span[normalize-space()='Local Purchases']"));
-        actions.moveToElement(localPurchase).pause(Duration.ofSeconds(2)).click();
-        ;
-        actions.pause(Duration.ofSeconds(2)).perform();
-        Thread.sleep(2000);
-        WebElement newLocalPurchase = driver.findElement(By.xpath("//button[normalize-space()='New Local Purchase']"));
-        actions.moveToElement(newLocalPurchase).pause(Duration.ofSeconds(2)).click();
-        actions.pause(Duration.ofSeconds(2)).perform();
+        element.selectElement(driver,actions,LocalPurchaseTestData.path2);
         Thread.sleep(2000);
 
-        //-----party/customer select
+        element.selectElement(driver,actions,LocalPurchaseTestData.path3);
+        Thread.sleep(2000);
+
+        //-----party/customer and product is select---//
+        DropDown dropdown = new DropDown();
+        dropdown.selectDropDown(driver,wait,1,LocalPurchaseTestData.partyIndex);
+        dropdown.selectDropDown(driver,wait,2,LocalPurchaseTestData.productIndex);
+
+        element.writeElement(driver,wait,LocalPurchaseTestData.challan_path).sendKeys(LocalPurchaseTestData.challanNo);
+        element.writeElement(driver,wait,LocalPurchaseTestData.vehicle_path).sendKeys(LocalPurchaseTestData.vehicleInfo);
+        Thread.sleep(2000);
+
+        String qty = Integer.toString(LocalPurchaseTestData.getRandomN());
+        element.writeElement(driver,wait,LocalPurchaseTestData.qty_path).sendKeys(qty);
+        Thread.sleep(2000);
+
+        String unitP = Integer.toString(LocalPurchaseTestData.unitP);
+        element.writeElement(driver,wait,LocalPurchaseTestData.unitP_path).sendKeys(unitP);
+        Thread.sleep(2000);
+        element.clickButton(driver,wait,LocalPurchaseTestData.save_btn_path);
+        Thread.sleep(2000);
+
+//        driver.navigate().back();
+//        Thread.sleep(3000);
+
+        String currentDate = LocalPurchaseTestData.date;
+        WebElement searchChallanDate = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//td[normalize-space()='"+currentDate+"']")
+        ));
 
 
+        if(searchChallanDate.isDisplayed())
+            System.out.println("record inserted successfully");
+        else System.out.println("Can not insert the record successfully");
         driver.quit();
     }
 }
--- src/test/java/nexVatAutomation/LocalPurchaseTestData.java
+++ src/test/java/nexVatAutomation/LocalPurchaseTestData.java
@@ -7,27 +7,41 @@ import java.time.format.DateTimeFormatter;
 import java.util.Random;
 
 public class LocalPurchaseTestData {
+
+    public static final String path1 = "//span[@title='Purchase']";
+    public static  final String path2 = "//span[normalize-space()='Local Purchases']";
+    public static final String path3 = "//button[normalize-space()='New Local Purchase']";
+
+    public static final String vehicle_path = "//input[@name='vehicle_info']";
+//    public static final String contact_path = "//input[@name='contact_no']";
+//    public static final String address_path = "//input[@name='address']";
+//    public static final String bin_path = "//input[@name='bin_or_nid']" ;
+    public static final String challan_path = "//input[@name='challan_no']";
+    public static final String save_btn_path = "//button[normalize-space()='Save']";
+    public static final String qty_path = "//input[@name='details.0.qty']";
+    public static final String unitP_path ="//input[@name='details.0.unit_price']";
+    public static final String search_path = "//input[@placeholder='Party, Challan No, Date']";
+
     static Random random = new Random();
     static int ch_num = random.nextInt(9999999);
     public static final String challanNo = "ch-" + System.currentTimeMillis()%100000;
-
+//    public static final String contact = "01" + System.currentTimeMillis()%1000000000;
     static int partyIndex = random.nextInt(10);
-    //public static final String partyInd = Integer.toString(partyIndex);
 
     static int vehicleNo = random.nextInt(2000);
     public static final String vehicleInfo = "DHAKA-Metro-A" + vehicleNo;
 
-    public static final String address = "Uttara,Dhaka";
+//    public static final String address = "Uttara,Dhaka";
 
     static int bin_part1 = 100000000 + random.nextInt(900000000);
     static int bin_part2 = random.nextInt(10000);
-    public static final String binNo = bin_part1 + "-" + bin_part2;
+//    public static final String binNo = bin_part1 + "-" + bin_part2;
 
-    static int productIndex = random.nextInt(15);
+    static int productIndex = random.nextInt(12);
 
-    static int randomN = random.nextInt(500)+ 1;
+    static int randomN = random.nextInt(500);
 
-    static  int qty;
+    static  int qty = randomN + 1;
 
     public static int getRandomN() {
         if(randomN%5==0 || randomN<=10) qty = randomN;
@@ -36,7 +50,7 @@ public class LocalPurchaseTestData {
         return qty;
     }
 
-    static int totalP = getRandomN()*randomN;
+    static int unitP = randomN;
 
     //get current date
     static LocalDate today = LocalDate.now();
@@ -49,9 +63,9 @@ public class LocalPurchaseTestData {
     void testAutomation(){
         //it shows unique time stamp after every Milli seconds. it contains 13 digits more
         System.out.println(System.currentTimeMillis());
-
+//        System.out.println(contact);
         int qty = getRandomN();
-        int totalPrice = qty*totalP;
+        int totalPrice = qty*unitP;
         System.out.println(qty);
         System.out.println(totalPrice);
 
