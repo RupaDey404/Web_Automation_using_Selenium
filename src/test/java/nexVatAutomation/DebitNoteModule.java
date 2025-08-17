aa7a990cddcfb98f0430e5f65d786be04d4d0b9d
RupaDey404 <rupa121353@gmail.com>
2025-08-17 17:52:07 +0600

    modified debitModule and debitTestData

--- src/test/java/nexVatAutomation/DebitNoteModule.java
+++ src/test/java/nexVatAutomation/DebitNoteModule.java
@@ -1,4 +1,63 @@
 package nexVatAutomation;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class DebitNoteModule {
+    @Test
+    void DebitNoteModuleAutomation() throws InterruptedException{
+        LoginPage loginPage = new LoginPage();
+        loginPage.login();
+
+        WebDriver driver = LoginPage.driver;
+        driver.manage().window().maximize();
+        Thread.sleep(2000);
+
+        Actions actions = new Actions(driver);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        selectWebElement element = new selectWebElement();
+        element.selectElement(driver,actions,LocalPurchaseTestData.path1);
+        Thread.sleep(2000);
+        element.selectElement(driver,actions,LocalPurchaseTestData.path2);
+        Thread.sleep(2000);
+
+        WebElement challanElement = driver.findElement(
+                By.cssSelector("table tr:first-child td:nth-child(3)")
+        );
+        String challanCopy = challanElement.getText();
+//        System.out.println(challanCopy);
+
+//        driver.switchTo().defaultContent();
+          driver.navigate().back();
+//        Debit Note starting:
+        element.selectElement(driver,actions,DebitNoteTestData.path1);
+
+        element.selectElement(driver,actions,DebitNoteTestData.path2);
+        element.selectElement(driver,actions,DebitNoteTestData.path3);
+
+        element.writeElement(driver,wait,DebitNoteTestData.challan_path).sendKeys(challanCopy).;
+        Thread.sleep(2000);
+
+        String debitNo = Integer.toString(DebitNoteTestData.debitNoteNo);
+
+        element.writeElement(driver,wait,DebitNoteTestData.debitNoteNo_path).sendKeys(debitNo);
+        Thread.sleep(2000);
+        element.writeElement(driver,wait,DebitNoteTestData.reason_path).sendKeys(DebitNoteTestData.reason);
+        Thread.sleep(2000);
+        element.clickButton(driver,wait,DebitNoteTestData.save_path);
+        Thread.sleep(2000);
+
+        wait.until(ExpectedConditions.urlContains("https://nexvat.com/debit-notes"));
+        Thread.sleep(2000);
+
+        driver.quit();
+    }
 }
--- src/test/java/nexVatAutomation/DebitNoteTestData.java
+++ src/test/java/nexVatAutomation/DebitNoteTestData.java
@@ -12,23 +12,41 @@ public class DebitNoteTestData {
     public static  final String path2 = "//span[normalize-space()='Debit Note']";
     public static final String path3 = "//button[normalize-space()='New Debit Note']";
 
+    public static final String challan_path = "//input[@placeholder='Press Enter or Focus Out']";
     public static final String debitNoteNo_path = "//input[@name='debit_note_no']" ;
     public static final String qty_path = "//input[@name='details.0.qty']";
     public static final String rtnQty_path = "//input[@name='details.0.return_qty']";
     public static final String reason_path = "//input[@name='details.0.remarks']";
+    public static final String save_path = "//button[normalize-space()='Save']";
 
     static Random random = new Random();
     static int debitNoteNo = random.nextInt(9999999);
-    static int qty, rtnQty = random.nextInt(10) ;
+    static int qty ;
+    static int  rtnQty = random.nextInt(qty%10) ;
     public static final String[] items = {"expire date","Damage material", "Label misprint",
                                         "Damaged packaging", "Customer cancellation", "Wrong batch delivered",
                                         "Seal broken", "Excess moisture detected"};
 
     static int reasonIndex = random.nextInt(items.length);
-    String reason = items[reasonIndex];
+    public static final String reason = items[reasonIndex];
 
+
+    public static int getRtnQty() {
+        if( qty>=1 && qty<=10)
+        {
+            if(rtnQty==0) {
+                rtnQty = rtnQty+1;
+            }
+            else if (rtnQty>1)
+                rtnQty = rtnQty-1;
+
+        }
+        return rtnQty;
+    }
     @Test
     void testAutomation(){
         System.out.println(reason);
+        System.out.println("return qty: "+rtnQty);
+        System.out.println(getRtnQty());
     }
 }
