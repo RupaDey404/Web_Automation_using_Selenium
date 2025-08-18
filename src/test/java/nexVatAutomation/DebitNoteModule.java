8ec9dc073bcea8e9d282b2b046be72571180765f
RupaDey404 <rupa121353@gmail.com>
2025-08-18 17:50:29 +0600

    here is added try catch to track the issued challan of debit note

--- src/test/java/nexVatAutomation/DebitNoteModule.java
+++ src/test/java/nexVatAutomation/DebitNoteModule.java
@@ -1,9 +1,6 @@
 package nexVatAutomation;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -26,55 +23,66 @@ public class DebitNoteModule {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
         selectWebElement element = new selectWebElement();
-//        element.selectElement(driver,actions,LocalPurchaseTestData.path1);
-//        Thread.sleep(2000);
-//        element.selectElement(driver,actions,LocalPurchaseTestData.path2);
-//        Thread.sleep(2000);
+        element.selectElement(driver,actions,LocalPurchaseTestData.path1);
+        Thread.sleep(2000);
+        element.selectElement(driver,actions,LocalPurchaseTestData.path2);
+        Thread.sleep(2000);
 
         WebElement challanElement = driver.findElement(
                 By.cssSelector("table tr:first-child td:nth-child(3)")
         );
         String challanCopy = challanElement.getText();
-//        System.out.println(challanCopy);
+        System.out.println(challanCopy);
 
-//        driver.switchTo().defaultContent();
-//          driver.navigate().back();
+        driver.switchTo().defaultContent();
+          driver.navigate().back();
 //        Debit Note starting:
         element.selectElement(driver,actions,DebitNoteTestData.path1);
 
         element.selectElement(driver,actions,DebitNoteTestData.path2);
-//        element.selectElement(driver,actions,DebitNoteTestData.path3);
-//
-//        element.writeElement(driver,wait,DebitNoteTestData.challan_path).sendKeys(challanCopy);
-//        Thread.sleep(2000);
-//
-//
-//        element.writeElement(driver,wait,DebitNoteTestData.debitNoteNo_path).sendKeys(DebitNoteTestData.debitNoteNo);
-//        Thread.sleep(2000);
-//
-//        String rtnQty = Integer.toString(DebitNoteTestData.getRtnQty());
-//        element.writeElement(driver,wait,DebitNoteTestData.rtnQty_path).sendKeys(rtnQty);
-//        Thread.sleep(2000);
-//        System.out.println("Return qty: " +rtnQty);
-//
-//        element.writeElement(driver,wait,DebitNoteTestData.reason_path).sendKeys(DebitNoteTestData.reason);
-//        Thread.sleep(2000);
-//
-//        element.clickButton(driver,wait,DebitNoteTestData.save_path);
-//        Thread.sleep(2000);
+        element.selectElement(driver,actions,DebitNoteTestData.path3);
 
-        wait.until(ExpectedConditions.urlContains("https://nexvat.com/debit-notes"));
-        Thread.sleep(2000);
+        element.writeElement(driver,wait,DebitNoteTestData.challan_path).sendKeys(challanCopy);
+        //Thread.sleep(2000);
 
-        WebElement debitNoteElement = driver.findElement(
-                By.cssSelector("tbody tr:nth-child(1) td:nth-child(3)")
-        );
-        String getTextDebitNo = debitNoteElement.getText();
-        System.out.println("debit note no: "+getTextDebitNo);
-        Assert.assertTrue(getTextDebitNo.matches("\\d+"),"64000464");
+        try {
+            WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofNanos(10));
+            WebElement alert = wait1.until(ExpectedConditions.visibilityOfElementLocated(
+                    By.xpath("//div[@class='toast toast-warning']")
+            ));
+            String getAlertText = alert.getText();
+            Assert.assertTrue(getAlertText.contains("Already exists a debit note"));
+            System.out.println("This challan has already been issued for debit note");
+
+        }
+        catch (TimeoutException e) {
+
+            element.writeElement(driver, wait, DebitNoteTestData.debitNoteNo_path).sendKeys(DebitNoteTestData.debitNoteNo);
+            Thread.sleep(2000);
+
+            String rtnQty = Integer.toString(DebitNoteTestData.getRtnQty());
+            element.writeElement(driver, wait, DebitNoteTestData.rtnQty_path).sendKeys(rtnQty);
+            Thread.sleep(2000);
+            System.out.println("Return qty: " + rtnQty);
+
+            element.writeElement(driver, wait, DebitNoteTestData.reason_path).sendKeys(DebitNoteTestData.reason);
+            Thread.sleep(2000);
+
+            element.clickButton(driver, wait, DebitNoteTestData.save_path);
+            Thread.sleep(2000);
+
+            wait.until(ExpectedConditions.urlContains("https://nexvat.com/debit-notes"));
+            Thread.sleep(2000);
+
+            WebElement debitNoteElement = driver.findElement(
+                    By.cssSelector("tbody tr:nth-child(1) td:nth-child(3)")
+            );
+            String getTextDebitNo = debitNoteElement.getText();
+            System.out.println("debit note no: " + getTextDebitNo);
+            Assert.assertTrue(getTextDebitNo.matches("\\d+"));
 
             System.out.println("Debit note created successfully");
-
+        }
         driver.quit();
     }
 }
